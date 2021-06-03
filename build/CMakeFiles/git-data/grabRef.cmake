a2958947965c9d08cc4d3d4650721e072d323480
Pham Long Huan <huanvip455@gmail.com>
2021-06-03 08:24:55 +0700

    fix2

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/Data/IOT/ThucHanh/Lab4/Code/lab4_2/lab4_2/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Users/Admin/esp/esp-idf/.git")
+set(GIT_DIR "D:/Data/IOT/ThucHanh/Lab4/Code/lab4_2/lab4_2/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
